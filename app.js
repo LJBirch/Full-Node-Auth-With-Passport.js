c9dabc42f613d7cdc15d7fd1ede577418190ea3b
Lewis <l.birch@lancaster.ac.uk>
2019-08-27 21:02:36 +0100

    Added ejs partials and templates.

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const PORT = process.env.PORT || 3000
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => res.render('index'))
+app.get('/auth/register', (req, res) => res.render('register'))
+
+app.post('/auth/register', (req, res) => res.send('Register!'))
 
 app.listen(PORT, () =>
   console.log(`Server is up and listening on port: ${PORT}`)

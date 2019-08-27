1107b8604b8554ed0b96d68c20a4a758f6d1e42b
Lewis <l.birch@lancaster.ac.uk>
2019-08-27 18:11:53 +0100

    Added basic server functionality.

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('Hello World!')
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(PORT, () =>
+  console.log(`Server is up and listening on port: ${PORT}`)
+)

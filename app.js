86edf4b1ed6601a22e7c01205171e3210b70899b
Lewis <l.birch@lancaster.ac.uk>
2019-08-28 16:19:06 +0100

    Added express sessions partial functionality.

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 3000
+const session = require('express-session')
+
+// Setup enviroment variables
+require('dotenv').config()
 
 // Connect to MongoDB
 require('./database/mongoose')
 
-// Setup View Engine
 app.set('view engine', 'ejs')
-
-// Setup BodyParser
 app.use(express.urlencoded({ extended: false }))
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+  })
+)
 
-// Use Routes
+// Auth Routes
 app.use('/auth', require('./routes/auth'))
 
 app.get('/', (req, res) => res.render('index'))
+
+// Setup port and listen
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () =>
   console.log(`Server is up and listening on port: ${PORT}`)
